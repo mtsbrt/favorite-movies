6a1fb6696110b543eec1f298e080b1eb3f089e29
Mateus Brito <mateus.brito01@hotmail.com>
2020-08-09 23:00:22 -0300

    Implementação da tela de perfil.

--- src/app/modules/profile/components/profile/profile.component.ts
+++ src/app/modules/profile/components/profile/profile.component.ts
@@ -1,12 +1,58 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.component.html',
     styleUrls: ['./profile.component.scss']
 })
+
+
 export class ProfileComponent implements OnInit {
+
+    public formProfile: FormGroup = this._formBuilder.group({
+        name: ['', Validators.required],
+        age: [''],
+        favoriteMovie: [''],
+        favoriteGenre: ['Horror']
+    });
+
+    public genres = ['Horror', 'Adventure', 'Sci-Fi', 'Fantasy', 'Romance', 'Comedy'];
+
+    constructor(
+        private _formBuilder: FormBuilder,
+        private _snackBar: MatSnackBar
+        ) { }
+
     public ngOnInit() {
+        this.carregarPerfil();
+    }
+
+    public carregarPerfil() {
+        const perfil = JSON.parse(localStorage.getItem('user'));
+
+        if (perfil) {
+            this.formProfile.controls.name.setValue(perfil.name);
+            this.formProfile.controls.age.setValue(perfil.age);
+            this.formProfile.controls.favoriteMovie.setValue(perfil.favoriteMovie);
+            this.formProfile.controls.favoriteGenre.setValue(perfil.favoriteGenre);
+        }
+
+    }
+
+    public saveProfile() {
+        const user = {
+            name: this.formProfile.controls.name.value,
+            age: this.formProfile.controls.age.value,
+            favoriteMovie: this.formProfile.controls.favoriteMovie.value,
+            favoriteGenre: this.formProfile.controls.favoriteGenre.value
+        };
         
+        localStorage.setItem('user', JSON.stringify(user));
+        this._snackBar.open('Profile saved successfully!', '', {
+            duration: 1000,
+            panelClass: 'success'
+        });
     }
 }
--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from "@angular/core";
+import { ProfileComponent } from './components/profile/profile.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
 
 @NgModule({
-    declarations: [],
-    imports: [],
+    declarations: [
+        ProfileComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatSnackBarModule
+    ],
     exports: []
 })
 export class ProfileModule { }
